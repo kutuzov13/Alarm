a5653ff373b1a0daab8df9e7017044e5d96dd5bd
Yegor <66310643+kutuzov13@users.noreply.github.com>
2020-10-11 20:58:11 +0300

    Create alarm.py

---
+++ alarm.py
@@ -0,0 +1,35 @@
+import time
+import os
+import threading
+
+class Alarm(threading.Thread):
+    def __init__(self, hours, minutes):
+        super(Alarm, self).__init__
+        self.hours = int(hours)
+        self.minutes = int(minutes)
+        self.keep_running = True
+
+    def run(self):
+        try:
+            while self.keep_running:
+                now = time.localtime()
+                if (now.tm_hour == self.hours and now.tm_min == self.minutes):
+                    time.localtime()
+                    print('Вставаааай!!!')
+                    os.popen(r'D:\Development\Udemy\The Weeknd - False Alarm.mp3')
+                    return
+        except:
+            return
+
+    def just_die(self):
+        self.keep_running = False
+
+name = input('Введите ваше имя: ')
+print(f'Привет {name}')
+
+alarm_HH = input("Введите час, в который вы хотите проснуться: ")
+alarm_MM = input("Введите минуту, в который вы хотите проснуться: ")
+print(f'Вы захотели проснуться в {alarm_HH}:{alarm_MM}')
+
+alarm = Alarm(alarm_HH, alarm_MM)
+alarm.start()
